2ff2d507d62af8eda9266b1f0a71c7e80d9a00fe
kunal prakash <kunalprakash1309@gmail.com>
2020-10-15 22:38:41 +0530

    added signup form for customer frontend (Kunal Prakash)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Display from './components/best_services/component.bestServices';
+import SignUpForCustomer from './components/signup_for_customer/signup_for_customer.components.jsx'
 
 function App() {
     return (
         <div>
             Hello client
             <Display />
+
+            {/* SignUpForCustomer Component (Kunal Prakash) */}
+            <SignUpForCustomer />
+            
         </div>
     );
 }
