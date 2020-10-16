8c96aa342c0332ff36f818a35bd1ec46a4839755
kunal prakash <kunalprakash1309@gmail.com>
2020-10-16 20:54:19 +0530

    Revert "added signup form for customer frontend (Kunal Prakash)"
    This reverts commit 2ff2d507d62af8eda9266b1f0a71c7e80d9a00fe.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Display from './components/best_services/component.bestServices';
-import SignUpForCustomer from './components/signup_for_customer/signup_for_customer.components.jsx'
 
 function App() {
     return (
@@ -8,10 +7,6 @@ function App() {
             Hello client
             {/* Our best services carousel (Kunal Prakash) */}
             <Display />
-
-            {/* SignUpForCustomer Component (Kunal Prakash) */}
-            <SignUpForCustomer />
-            
         </div>
     );
 }
