0387784c8f2de4c7223f39ee91b5161ee853422b
naveen-droid <naveen.cs18@nitp.ac.in>
2020-10-18 00:44:15 +0530

    Footer component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Display from './components/best_services/component.bestServices';
+import React from "react";
+import Display from "./components/best_services/component.bestServices";
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-            Hello client
-            {/* Our best services carousel (Kunal Prakash) */}
-            <Display />
-        </div>
-    );
+  return (
+    <div>
+      Hello client
+      {/* Our best services carousel (Kunal Prakash) */}
+      <Display />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
