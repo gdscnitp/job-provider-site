afac088247bd093edd89ae6a08b1d17a55bb2701
Divya Prakash <medivyaprakash@gmail.com>
2020-10-18 21:47:44 +0530

    prettier added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
+import React from "react";
+import Display from "./components/best_services/component.bestServices";
+import SignUpForCustomer from "./components/signup_for_customer/signup_for_customer.components.jsx";
 
-import React from 'react';
-import Display from './components/best_services/component.bestServices';
-import SignUpForCustomer from './components/signup_for_customer/signup_for_customer.components.jsx'
-  
 import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-            Hello client
-            {/* Our best services carousel (Kunal Prakash) */}
-            <Display />
-
-            {/* SignUp form for customer (Kunal Prakash) */}
-            <SignUpForCustomer />
-      
-      
-       <Footer />
-        </div>
-    );
-
+  return (
+    <div>
+      Hello client
+      {/* Our best services carousel (Kunal Prakash) */}
+      <Display />
+      {/* SignUp form for customer (Kunal Prakash) */}
+      <SignUpForCustomer />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
