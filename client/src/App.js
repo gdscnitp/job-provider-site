3107b25bc3cd99b6e2594129a1b0ee2677a90a56
Saurabhdhotre2709 <saurabhdhotre04@gmail.com>
2020-10-19 22:27:59 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Navbar from'./components/Navbar';
+import './components/Navbar.css';
 function App() {
   return (
-    <div>
-      Hello client
-    </div>
+   <div>
+<Navbar/>
+   </div>
   );
 }
 
