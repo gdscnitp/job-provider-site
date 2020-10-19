a2097ade019e25bb9cacd7f9fa47449d381acaf5
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-19 19:51:22 +0530

    Added my Work

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Display from './components/best_services/component.bestServices';
+import SignUpForCustomer from './components/signup_for_customer/signup_for_customer.components.jsx'
 
 function App() {
-  return (
-    <div>
-      Hello client
-    </div>
-  );
+    return (
+        <div>
+            Hello client
+            {/* Our best services carousel (Kunal Prakash) */}
+            <Display />
+
+            {/* SignUp form for customer (Kunal Prakash) */}
+            <SignUpForCustomer />
+        </div>
+    );
 }
 
 export default App;
