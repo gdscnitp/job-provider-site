b6daeb014a682be12acb63f872e002b7caf4c52d
Saurabhdhotre2709 <saurabhdhotre04@gmail.com>
2020-10-22 01:20:46 +0530

    All required Changes Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-
 import React from "react";
 import Display from "./components/best_services/component.bestServices";
-import Navbar from'./components/Navbar';
+import Navbar from "./components/Navbar";
 import SignUpForCustomer from "./components/signup_for_customer/signup_for_customer.components.jsx";
 
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
+    <div>
+      <Navbar />
       {/* Our best services carousel (Kunal Prakash) */}
       <Display />
       {/* SignUp form for customer (Kunal Prakash) */}
       <SignUpForCustomer />
       <Footer />
     </div>
-
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,52 @@
-import React from 'react';
-import * as ReactBootStrap from 'react-bootstrap';
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
 
 function Navbar() {
   return (
     <div>
-<ReactBootStrap.Navbar  expand="lg" bg="primary" variant="dark" className= 'navbar'>
-  <ReactBootStrap.Navbar.Brand href="#home" className="brand-logo">WorkLink</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
-    <ReactBootStrap.Nav className="ml-auto">
-      <ReactBootStrap.Nav.Link href="#home" className="nav-option">Home</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#link" className="nav-option">Contact Us</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Other Links" id="basic-nav-dropdown" className="nav-option">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Pricing</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Button variant="success">Sign in</ReactBootStrap.Button>
-  </ReactBootStrap.Navbar.Collapse>
-</ReactBootStrap.Navbar>
-
+      <ReactBootStrap.Navbar
+        expand="lg"
+        bg="primary"
+        variant="dark"
+        className="navbar"
+      >
+        <ReactBootStrap.Navbar.Brand href="#home" className="brand-logo">
+          WorkLink
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Nav className="ml-auto">
+            <ReactBootStrap.Nav.Link href="#home" className="nav-option">
+              Home
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#link" className="nav-option">
+              Contact Us
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.NavDropdown
+              title="Other Links"
+              id="basic-nav-dropdown"
+              className="nav-option"
+            >
+              <ReactBootStrap.NavDropdown.Item href="#action/3.1">
+                Pricing
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item href="#action/3.2">
+                Another action
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item href="#action/3.3">
+                Something
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Divider />
+              <ReactBootStrap.NavDropdown.Item href="#action/3.4">
+                Separated link
+              </ReactBootStrap.NavDropdown.Item>
+            </ReactBootStrap.NavDropdown>
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Button variant="success">
+            Sign in
+          </ReactBootStrap.Button>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
     </div>
   );
 }
