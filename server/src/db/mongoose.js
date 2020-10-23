4b3acca4b5d87b6c6eec8d5541765652d101df2e
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-23 18:45:27 +0530

    CHanges

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,17 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// DataBase Connections
-// mongoose
-// 	.createConnection(process.env.WorkersDB, {
-// 		useNewUrlParser: true,
-// 		useCreateIndex: true,
-// 		useFindAndModify: false,
-// 		useUnifiedTopology: true,
-// 	})
-// 	.then(() => console.log(`Workers Database connected successfully`))
-// 	.catch((err) => console.log(err));
-
 mongoose
 	.connect(process.env.MainDB, {
 		useNewUrlParser: true,
--- server/src/routers/api.js
+++ server/src/routers/api.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const Customer = require('../models/Customer');
 
 // Testing Purpose
-router.get('/signup_customer', (req, res) => {
-	Customer.find({}).then((data) => res.json(data));
-});
+// router.get('/signup_customer', (req, res) => {
+// 	Customer.find({}).then((data) => res.json(data));
+// });
 
 router.use(express.json());
 
 router.post('/signup_customer', (req, res, next) => {
 	Customer.create(req.body)
-		.then((data) => res.json(data))
+		.then((data) => console.log('SuccessFully Saved'))
 		.catch(next);
 });
 
