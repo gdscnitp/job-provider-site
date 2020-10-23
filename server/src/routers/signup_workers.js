1fe7218a872eec1d3dcc78ca477db3b9b788bf82
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-23 19:16:58 +0530

    Final Changes

--- server/src/routers/signup_workers.js
+++ server/src/routers/signup_workers.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const Worker = require('../models/signup_workers')
-const router = new express.Router()
-
+const express = require('express');
+const Worker = require('../models/signup_workers');
+const router = new express.Router();
 
 router.post('/workers', async (req, res) => {
-    const worker = new Worker(req.body)
-    
-    try {
-        if(req.body.password !== req.body.confirm_password){
-            return res.status(400).send('Password does not match')
-        }
-        await worker.save()
-        
-        const token = await worker.generateAuthToken()
-        //res.json(worker)
-        res.status(201).send({ worker, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+	const worker = new Worker(req.body);
 
-module.exports = router
+	try {
+		if (req.body.password !== req.body.confirm_password) {
+			return res.status(400).send('Password does not match');
+		}
+		await worker.save().then(() => console.log('Successfully Done'));
+
+		const token = await worker.generateAuthToken();
+		//res.json(worker)
+		res.status(201).send({ worker, token });
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+module.exports = router;
