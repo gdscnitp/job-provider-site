f1be372688481d84d5e89f2fc04edf2326197a1a
Digvijay Srivastava <dig9074vijay@gmail.com>
2020-10-24 01:18:58 -0700

    Added SignUp For Worker

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Display from "./components/best_services/component.bestServices";
 import Navbar from "./components/Navbar/Navbar";
 import SignUpForCustomer from "./components/signup_for_customer/signup_for_customer.components.jsx";
+import SignUpForWorker from "./components/signup_for_worker/signup_for_worker.jsx";
 import VideoDisplay from "./components/videoDisplay/videoDisplay.js";
 
 import Footer from "./components/Footer/Footer";
@@ -12,6 +13,8 @@ function App() {
       <Navbar />
       {/* Our best services carousel (Kunal Prakash) */}
       <Display />
+      {/* SignUp form for customer (Kunal Prakash) */}
+      <SignUpForWorker />
       {/* SignUp form for customer (Kunal Prakash) */}
       <SignUpForCustomer />
       {/* video component for front page(Ritesh Singh) */}
