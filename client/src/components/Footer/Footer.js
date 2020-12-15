65bfa0857fadf094febc8e115cae5924e210ce3f
PawanSingh <pmarsingh@gmail.com>
2020-12-15 14:19:06 +0530

    Added Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,93 @@
 import React from "react";
+import {
+  FaFacebook,
+  FaGooglePlus,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="main-footer">
-      <center>
-        <h1>Footer Section</h1>
-      </center>
+    <div className="main-footer page-footer">
+      <div className="row ">
+        <div className="col-md-2 ">
+          <center>
+            <h5 className="mb-2 pb-2">Cities we serve</h5>
+            <ul className="list-unstyled">
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+            </ul>
+          </center>
+        </div>
+        <div className="col-md-2">
+          <center>
+            <h5 className="mb-2 pb-2">Cities we serve</h5>
+            <ul className="list-unstyled">
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+            </ul>
+          </center>
+        </div>
+        <div className="col-md-2">
+          <center>
+            <h5 className="mb-2 pb-2">Cities we serve</h5>
+            <ul className="list-unstyled">
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+              <li>ABC</li>
+            </ul>
+          </center>
+        </div>
+        <div className="col-md-6">
+          <center>
+            <h5 className="mt-4 mb-4">Find us on:</h5>
+            <ul class="list-unstyled list-inline text-center">
+              <li class="list-inline-item mx-2">
+                <a
+                  class="btn-floating btn-fb text-white "
+                  href="https://www.facebook.com/"
+                >
+                  <FaFacebook size="4rem" />
+                </a>
+              </li>
+              <li class="list-inline-item mx-2">
+                <a
+                  class="btn-floating btn-tw text-white"
+                  href="https://www.twitter.com/"
+                >
+                  <FaTwitter size="4rem" />
+                </a>
+              </li>
+              <li class="list-inline-item  mx-2 ">
+                <a
+                  class="btn-floating btn-gplus text-white"
+                  href="https://www.gplus.com/"
+                >
+                  <FaGooglePlus size="4rem" />
+                </a>
+              </li>
+              <li class="list-inline-item mx-2">
+                <a
+                  class="btn-floating btn-li text-light"
+                  href="https://www.linkedin.com/"
+                >
+                  <FaLinkedin size="4rem" />
+                </a>
+              </li>
+            </ul>
+          </center>
+        </div>
+      </div>
+
+      <div class="text-center py-3" style={{ fontSize: "1.5rem " }}>
+        @Worklink, All rights reserved
+      </div>
     </div>
   );
 }
